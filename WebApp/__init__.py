c81aebf18e2611caa6516ee90fe305412d8de7df
jabez007 <jabez.m.007@gmail.com>
2017-03-27 15:44:20 -0500

    removing use of builtin app logger

--- WebApp/__init__.py
+++ WebApp/__init__.py
@@ -4,8 +4,8 @@ from Log import MyLog
 app_logger = MyLog(name=__name__)
 app = Flask(__name__)
 app.config.from_object('webapp_config')
-for hndlr in app_logger.handlers:
-    app.logger.addHandler(hndlr)
+# for hndlr in app_logger.handlers:
+    # app.logger.addHandler(hndlr)
 app_logger.info(app.secret_key)
 
 from WebApp import views
